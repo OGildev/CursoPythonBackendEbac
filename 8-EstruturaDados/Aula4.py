21e9516fd65c7b613cef35cc698ffccc3391bddd
gilberto <gilbertolima3004@gmail.com>
2026-05-20 18:08:31 -0300

    Aula 4 - Utilizando a função split() para separar o conteúdo de strings!

---
+++ 8-EstruturaDados/Aula4.py
@@ -0,0 +1,9 @@
+
+# Dada uma string s que consiste em palavras e espaços, retorne o comprimento da última palavra da string.
+
+string = "    Fly me    to moon   "
+
+array = string.split()
+
+print(array)
+print(len(array[-1]))
