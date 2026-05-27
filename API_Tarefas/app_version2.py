9818b53d763e09310776150bddb58ea8ab6854d0
gilberto <gilbertolima3004@gmail.com>
2026-05-27 20:28:40 -0300

    Implementando autenticação por usuário e senha na API de Tarefas!

--- API_Tarefas/app_version2.py
+++ API_Tarefas/app_version2.py
@@ -1,10 +1,37 @@
 
 
-
+from fastapi import FastAPI, HTTPException, Depends # type: ignore
+from fastapi.security import HTTPBasic, HTTPBasicCredentials # type: ignore
 from fastapi import FastAPI, HTTPException  # type: ignore
 from pydantic import BaseModel # type: ignore
+import secrets
+import os
 
-app = FastAPI()
+app = FastAPI(
+    title="API de Tarefas",
+    description="API para gerenciar tarefas diárias",
+    version="1.0.0",
+    contact={
+        "name":"Gilberto Lima",
+        "email":"ogildev@gmail.com"
+    }
+)
+
+USUARIO = "admin"
+SENHA = "admin"
+
+security = HTTPBasic()
+
+def autenticacao(credentials: HTTPBasicCredentials = Depends(security)):
+    is_username_correct = secrets.compare_digest(credentials.username, USUARIO)
+    is_password_correct = secrets.compare_digest(credentials.password, SENHA)
+
+    if not (is_username_correct and is_password_correct):
+        raise HTTPException(
+            status_code=401,
+            detail="Usuário ou senha incorretos",
+            headers={"WWW-Authenticate": "Basic"},
+        )
 
 class Tarefa(BaseModel):
     nome: str
@@ -18,14 +45,14 @@ tarefas = [
 ]
 
 @app.get("/tarefas")
-def get_tarefas():
+def get_tarefas(credentials: HTTPBasicCredentials = Depends(autenticacao)):
     if not tarefas:
         return {"message": "Não existe essa tarefa."}
     else:
         return {"tarefas": tarefas}
     
 @app.post("/adicionar")
-def post_tarefas(tarefa: Tarefa):
+def post_tarefas(tarefa: Tarefa, credentials: HTTPBasicCredentials = Depends(autenticacao)):
     for t in tarefas:
         if t.nome == tarefa.nome:
             raise HTTPException(status_code = 400, detail = "Essa tarefa já existe!")
@@ -35,7 +62,7 @@ def post_tarefas(tarefa: Tarefa):
     return{"message": "A tarefa foi adicionada com sucesso!"}
 
 @app.put("/atualizar/{nome}")
-def put_tarefa(nome: str):
+def put_tarefa(nome: str, credentials: HTTPBasicCredentials = Depends(autenticacao)):
     for t in tarefas:
         if t.nome == nome:
             t.concluida = True
@@ -44,7 +71,7 @@ def put_tarefa(nome: str):
     raise HTTPException(status_code = 400, detail = "Essa tarefa não existe para ser atualizada!")    
 
 @app.delete("/deletar/{nome}")
-def delete_tarefa(nome: str):
+def delete_tarefa(nome: str, credentials: HTTPBasicCredentials = Depends(autenticacao)):
     for tarefa in tarefas:
         if tarefa.nome == nome:
             tarefas.remove(tarefa)
